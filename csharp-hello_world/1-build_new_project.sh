57bd5db4f49a231fb97854077ffbb990db192499
Teylor Chapman <5535@holbertonstudents.com>
2023-05-11 12:54:10 -0500

    maybe this will give me the correct output

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # this is gonna initialize a new project
 
-dotnet new console -o 1-new_project -f "netcoreapp2.1"
+dotnet new console -o 1-new_project
 dotnet build "1-new_project"
