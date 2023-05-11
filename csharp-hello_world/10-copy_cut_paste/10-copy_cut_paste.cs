591ea321b4c3bf6a9b2d359ff95776ee317585dc
Teylor Chapman <5535@holbertonstudents.com>
2023-05-11 14:48:12 -0500

    again with the capitals :/

--- csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -5,9 +5,9 @@ class Program
         static void Main(string[] args)
         {
 		string word = "Holberton";
-        string word_first_3, word_last_2, middle_word = word.Substring(1, word.length - 2);
+        string word_first_3, word_last_2, middle_word = word.Substring(1, word.Length - 2);
         word_first_3 = word.Substring(1, 3);
-        word_last_2 = word.Substring(word.length - 2);
+        word_last_2 = word.Substring(word.Length - 2);
 		Console.WriteLine("First 3 letters: {0}", word_first_3);
 		Console.WriteLine("Last 2 letters: {0}", word_last_2);
 		Console.WriteLine("Middle word: {0}", middle_word);
