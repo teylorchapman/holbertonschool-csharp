2f5807bf502ed6436f09b6d78de73cd0f3aadb27
Teylor Chapman <5535@holbertonstudents.com>
2023-05-11 14:11:55 -0500

    print the strings

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        Console.WriteLine(string.Concat(str, str, str));
+        Console.Writeline(str.Substring(0, 9));
+        }
+}
