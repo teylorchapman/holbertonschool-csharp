f839627a8eeddc757323038c89c1f5190c397c21
Teylor Chapman <5535@holbertonstudents.com>
2023-05-11 14:36:52 -0500

    specs

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+        Console.WriteLine("Percent: {0}", percent.ToString("0.00%"));
+        Console.WriteLine("Currency: {0}", currency.ToString("C", new System.Globalization.CultureInfo("en-US")));
+	}
+}
