4bcf190f854e565e2578d606367b90a99eb8f449
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 14:09:43 -0500

    this is the last digit that we're ever gonna HAVE

---
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int digit = number % 10;
+        if (digit == 0)
+            Console.WriteLine("The last digit of {0} is {1} and is 0", number, digit);
+        else if (digit < 5)
+            Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number, digit);
+        else
+            Console.WriteLine("The last digit of {0} is {1} and is greater than 5", number, digit);
+    }
+}
