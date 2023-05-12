ba86ba5b3a262eb20483c9be3b45a2d7717f8e18
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 16:36:43 -0500

    comb

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class hexed
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i <= 99; i++)
+        {
+            if (i < 99)
+                Console.Write($"{i.ToString("D2")}, ");
+            else
+                Console.Write($"{i.ToString("D2")}\n");
+        }
+    }
+}
