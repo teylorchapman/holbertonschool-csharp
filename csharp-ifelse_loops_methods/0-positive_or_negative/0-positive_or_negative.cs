28eab4f516a6025396269623b97be45d8808e21f
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 13:41:24 -0500

    positive or negative

---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        if(num < 0)
+        {
+            Console.WriteLine("{0} is negative", num);
+            return;
+        }
+        if(num > 0)
+        {
+            Console.WriteLine("{0} is positive", num);
+            return;
+        }
+        Console.WriteLine("{0} is zero", num);
+    }
+}
