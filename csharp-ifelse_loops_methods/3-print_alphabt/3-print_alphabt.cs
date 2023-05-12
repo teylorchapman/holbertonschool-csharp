cda3916c4afa901ff47114e2db9859cc303aa392
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 14:32:06 -0500

    alphabet w/o e & q

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Alphabet
+{
+    public static void Main(string[] args)
+    {
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            if (c == 'e' || c == 'q')
+                continue;
+            Console.Write(c);
+        }
+    }
+}
