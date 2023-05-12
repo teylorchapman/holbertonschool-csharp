fe48b356ee225dcb67f49f2f446ff3544d352046
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 16:48:52 -0500

    the second combo

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class hexed
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i <=8; i++)
+        {
+            for (int j = i + 1; j <= 9; j++)
+            {
+                if (i < 8)
+                    Console.Write($"{i}{j}, ");
+                else
+                    Console.Write($"{i}{j}\n");
+            }
+        }
+    }
+}
