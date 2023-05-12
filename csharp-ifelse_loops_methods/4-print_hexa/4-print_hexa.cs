d84927dd8c542afe5a1db844661dcd7f5ef31764
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 16:23:00 -0500

    print hexas

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class hexed
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                Console.WriteLine("{0} = 0x{1}", i, i.ToString("x"));
+            }
+        }
+    }
