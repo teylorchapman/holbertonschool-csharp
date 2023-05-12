124f16478972aad7d6cc27f970f37df8b2d59467
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 18:31:42 -0500

    print a line

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Line
+{
+    public static void PrintLine(int length)
+    {
+        for (i = o; i < length; i++)
+            Console.Write("_");
+        Console.WriteLine();
+    }
+}
