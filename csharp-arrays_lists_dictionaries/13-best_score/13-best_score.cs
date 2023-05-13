c469984a52121eaf901ec95f85b9455e7c05f9d1
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 23:16:44 -0500

    best score hopefully

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myDict.Count == 0)
+        {
+            return "None";
+        }
+
+        string maxkey = null;
+        int maxvalue = -1;
+
+        foreach (var pair in myDict)
+        {
+            if (pair.Value > maxvalue)
+            {
+                maxvalue = pair.Value;
+                maxkey = pair.Key;
+            }
+        }
+        return maxkey;
+    }
+}
