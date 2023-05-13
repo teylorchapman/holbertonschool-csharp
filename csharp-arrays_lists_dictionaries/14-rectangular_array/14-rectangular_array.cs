4a816437769a8e5649c5b0516e207d6e12b2e2cd
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 23:33:38 -0500

    hope

--- csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class spongebox
+public class OggBox
 {
     public static void Main(String[] args)
     {
-        int[] myArray = new int[5, 5];
+        int[,] myArray = new int[5, 5];
         myArray[2, 2] = 1;
-        for (int i = 0; i < 5; i++)
+        for(int i = 0; i < 5; i++)
         {
-            for(int = 0; j < 5; j++)
+            for(int j = 0; j < 5; j++)
             {
                 Console.Write(myArray[i, j]);
-                if (j < 4)
+                if(j < 4)
                     Console.Write(" ");
             }
-            Console.Write();
+            Console.WriteLine();
         }
     }
 }
