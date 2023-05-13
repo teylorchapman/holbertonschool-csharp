4bbf82fcb53f8d9f791d1c671c53f3f912b46503
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 21:34:54 -0500

    delete an item at a specific location in a list

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> DeleteAt(List<int> myList, in index)
+    {
+        if (index < 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+        myList.Remove(myList[index]);
+        return myList;
+    }
+}
