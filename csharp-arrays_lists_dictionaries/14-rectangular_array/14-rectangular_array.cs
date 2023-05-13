e2e4c4638cfa31b03982dc1c9ab72ea89797d058
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 23:30:12 -0500

    rectangle 5x5 or summ

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class spongebox
+{
+    public static void Main(String[] args)
+    {
+        int[,] myArray = new int[5, 5];
+        myArray[2, 2] = 1;
+        for (int i = 0; i < 5; i++)
+        {
+            for(int = 0; j < 5; j++)
+            {
+                Console.Write(myArray[i, j]);
+                if (j < 4)
+                    Console.Write(" ");
+            }
+            Console.Write();
+        }
+    }
+}
