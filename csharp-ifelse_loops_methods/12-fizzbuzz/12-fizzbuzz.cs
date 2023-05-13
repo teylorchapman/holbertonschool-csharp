782ba3ff2792b099771488433cf5f2d249d343c9
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 19:17:22 -0500

    good ol fizzbuzz

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class FiZzBuZz
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.Write("Fizz ");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.Write("Buzz ");
+            }
+            else if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.Write("FizzBuzz ");
+            }
+            else
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
