c9ff75473ef02f6610fd25c05ea411a82df77e8e
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 22:38:11 -0500

    maybe this time

--- csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 
 public class Dictionary
 {
-    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value);
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
     {
         myDict[key] = value;
         return myDict;
     }
 }
- 
