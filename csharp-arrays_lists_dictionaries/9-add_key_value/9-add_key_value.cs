a790c944f7d95afa332f3f61c974cfdf66d1e91f
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 22:32:04 -0500

    im tired

--- csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -5,10 +5,8 @@ public class Dictionary
 {
     public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value);
     {
-        if (myDict.ContainsKey(key))
-            myDict[key] = value;
-        else
-            myDict.Add(key, value);
+        myDict[key] = value;
         return myDict;
     }
 }
+ 
