668ae2a90cee02f1e136cc5f2104df2893d27d74
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 20:51:31 -0500

    print list

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -9,13 +9,13 @@ public class List
             Console.WriteLine("Size cannot be negative");
             return null;
         }
-        List<int> lt = new List<int>();
+        List<int> list = new List<int>();
         for (int i = 0; i < size; i++)
         {
-            lt.Add(i);
-            Console.Write(i + (i != size -1 ? "" : " "));
+            list.Add(i);
+            Console.Write(i + (i != size - 1 ? " " : ""));
         }
         Console.WriteLine();
-        return lt;
+        return list;
     }
 }
