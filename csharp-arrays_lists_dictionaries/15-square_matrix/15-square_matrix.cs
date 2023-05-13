3c577299c82544c9094c79e9184b93dfbcd11290
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 23:49:16 -0500

    its okay

--- csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -4,19 +4,11 @@ public class MaTrIx;
 {
     public static int [,], Square(int[,[ myMatrix]])
     {
-        int rows = myMatrix.GetLength(0);
-        int clms = myMatrix.GetLength(1);
-        int[,] squaredMatrix = new int[rows, clms];
-
-        for ( i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < clms; j++)
-            {
-                int origValue = myMatrix[i, j];
-                int squaredValue = origValue * origValue;
-                squaredMatrix[i, j] = squaredValue;
-            }
-        }
-        return squaredMatrix;
+        int[,] = myMatrix.GetLength(0);
+        int[,] = myMatrix.GetLength(1);
+        for(int i = 0; i < myMatrix.GetLength(0); i++)
+            for(int j = 0; j < myMatrix.GetLength(1); j++)
+                squareMatrix[i, j] = myMatrix[i, j] * myMatrix[i, j];
+        return squareMatrix;
     }
 }
