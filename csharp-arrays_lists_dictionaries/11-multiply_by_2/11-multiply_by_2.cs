ba7ab8c7679563051cebf9ad2b73a24149e9d0f1
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 22:53:51 -0500

    multiple

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> newDict = new Dictionary<string, int>();
+        foreach (var pair in myDict)
+        {
+            newDict[pair.Key] = pair.Value * 2;
+        }
+        return newDict;
+    }
+}
