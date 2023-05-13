1ef243fa18e01d2e8d573c9c2f6919b093120842
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 20:35:22 -0500

    esrever ni detnirp

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,6 +4,12 @@ public class Array
 {
     public static void Reverse(int[] array)
     {
+        if (array == null)
+        {
+            Console.WriteLine();
+            return;
+        }
+
         for (int i = array.Length - 1; i >= 0; i--)
         {
             Console.Write(array[i] + (i != 0 ? " " : ""));
