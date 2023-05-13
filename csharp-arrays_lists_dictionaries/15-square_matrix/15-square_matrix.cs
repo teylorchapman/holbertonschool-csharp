8d41eb017b5fb090e55c14d8d6251c8fd143f06a
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 23:45:07 -0500

    squared matrix bestie

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class MaTrIx;
+{
+    public static int [,], Square(int[,[ myMatrix]])
+    {
+        int rows = myMatrix.GetLength(0);
+        int clms = myMatrix.GetLength(1);
+        int[,] squaredMatrix = new int[rows, clms];
+
+        for ( i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < clms; j++)
+            {
+                int origValue = myMatrix[i, j];
+                int squaredValue = origValue * origValue;
+                squaredMatrix[i, j] = squaredValue;
+            }
+        }
+        return squaredMatrix;
+    }
+}
