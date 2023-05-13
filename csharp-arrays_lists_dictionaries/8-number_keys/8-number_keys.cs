7cdcf1d60df84b283d899dd0b1df038582be98e6
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 22:10:59 -0500

    i really want to finish this one at least please

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static int NumberOfKeys(Dictionary < string, string > myDict)
+    {
+        int x = 0;
+        foreach(string key in myDict.Keys)
+            x++;
+        return x;
+    }
+}
