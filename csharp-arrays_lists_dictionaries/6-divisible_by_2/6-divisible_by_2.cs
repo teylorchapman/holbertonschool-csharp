834499a24bb1298b98006bec2d0be98202e27527
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 21:19:05 -0500

    multiple of 2

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> sum = new List<bool>();
+        foreach (int i in myList)
+            sum.Add(i % 2 == 0);
+        return sum;
+    }
+}
