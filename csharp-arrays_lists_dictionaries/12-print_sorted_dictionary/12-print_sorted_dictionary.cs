a55b96cb7473d3feef26b83d492a0db3922f78dd
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 23:07:38 -0500

    sorted

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> keys = new list<string>(myDict.Keys);
+        keys.Sort();
+
+        foreach (string key in keys)
+        {
+            Console.WriteLine($"{key}: {myDict[key]}");
+        }
+    }
+}
