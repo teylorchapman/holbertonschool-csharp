fde95121fd352593548db324946563b0088ea263
Teylor Chapman <5535@holbertonstudents.com>
2023-05-13 18:14:31 -0500

    unique add

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> unique = new HashSet<int>();
+        int sum = 0;
+
+        foreach (int i in myList)
+        {
+            if (!unique.Contains(i))
+            {
+                unique.Add(i);
+                sum += i;
+            }
+        }
+    }
+    return sum;
+}
---
+++ csharp-hashset_stack_queue_linkedlist/README.md
@@ -0,0 +1,1 @@
+This is the Data Structures: HashSet, Stack, Queue, LinkedList
