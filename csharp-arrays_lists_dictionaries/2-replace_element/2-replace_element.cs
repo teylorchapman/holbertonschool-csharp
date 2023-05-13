f652db55eae79728a13a7f94e05b64f327e0e552
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 20:15:16 -0500

    replace these arrays

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -2,7 +2,7 @@
 
 public class Array
 {
-    public static int ReplaceElement(int[] array, int index, int n)
+    public static int[] ReplaceElement(int[] array, int index, int n)
     {
         if (index < 0 || index >= array.Length)
         {
