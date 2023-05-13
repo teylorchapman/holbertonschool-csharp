f0602d36c1362f03e2622f9657cb403e9fb120ea
Teylor Chapman <5535@holbertonstudents.com>
2023-05-12 21:04:14 -0500

    max integers

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList == null)
+        {
+            Console.WriteLine("List is empty")
+            return -1;
+        }
+        int max = myList[0];
+        for (int i = 1, i < myList.Count; i++)
+        {
+            if (myList[i] > max)
+            {
+                max = myList[i];
+            }
+        }
+        return max;
+    }
+}
