c88740e6d76234df560ec64b9dacff38ecdf2085
Teylor Chapman <5535@holbertonstudents.com>
2023-05-14 10:42:21 -0500

    common elements

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+pubic class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set 2 = new HashSet<int>(list);
+        HashSet<int> sum = new List<int>();
+
+        foreach (var common in set1)
+        {
+            if (set2.Contains(common))
+            {
+                sum.Add(common);
+            }
+        }
+
+        for (int x = 0; i < common.Count -1; x++)
+        {
+            for (int y = x + 1; y < common.Count; y++)
+            {
+                if (common[y] < common[x])
+                {
+                    int temp = common[x]
+                    common[x] = common[y];
+                    common[y] = temp;
+
+                }
+            }
+        }
+        return common;
+    }
+}
