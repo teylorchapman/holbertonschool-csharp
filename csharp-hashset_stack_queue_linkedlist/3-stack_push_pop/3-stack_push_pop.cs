ff87c58824a8ba6472d94d8f41af1cff4ce324c0
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 17:16:09 -0500

    mayhebs

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -3,24 +3,32 @@ using System.Collections.Generic;
 
 public class MyStack
 {
+    //is it comments thats making this thing angy??
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
+        //print count
         Console.WriteLine("Number of items: {0}" + aStack.Count);
 
+    
+        //prints whether or not the stack is empy
         if (aStack.Count > 0)
             Console.WriteLine("Top item: {0}" + aStack.Peek());
         else
             Console.WriteLine("Stack is empty");
-    }
+    
 
-     bool containsSearch = aStack.Contains(search);
-        Console.WriteLine("Stack contains \"{0}\": {1}", search, containsSearch);
+        //print if exsist
+        bool containsSearch = aStack.Contains(search);
+            Console.WriteLine("Stack contains \"{0}\": {1}", search, containsSearch);
 
-    if (containsSearch)
-    {
-        while (aStack.Pop() != search)
-            continue;
+        //pop, lock, and drop it
+        if (containsSearch)
+        {
+            while (aStack.Pop() != search)
+                continue;
+        }
+        //ah ssss push it
+        aStack.Push(newItem);
+        return aStack;
     }
-    aStack.Push(newItem);
-    return aStack;
 }
