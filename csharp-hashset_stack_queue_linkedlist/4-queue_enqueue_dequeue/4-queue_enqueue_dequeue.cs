d32be6104fff3fef80f51be0ffbb102783ded91c
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 17:30:04 -0500

    dequeue this

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: "),  aQueue.Count;
+        
+        if (aQueue.Count > 0)
+            Console.WriteLine("Top item: " + aQueue.Count);
+        else
+            Console.WriteLine("Stack is empty");
+        
+        bool containsSearch = aQueue.Countains(search);
+        Console.WriteLine("Queue contains \"{0}\: {1}", search, containsSearch);
+        
+        if (containsSearch)
+        {
+            while (aQueue.Dequeue() != search)
+                continue;
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
