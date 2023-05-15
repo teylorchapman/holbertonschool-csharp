cbb660ba2845ed0a0c4ffb8bfd00fd5bfc81ee48
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 17:07:45 -0500

    also no, *finger guns in sadness*

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,16 +5,16 @@ public class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        Console.WriteLine("Number of items: " + aStack.Count);
+        Console.WriteLine("Number of items: {0}" + aStack.Count);
 
         if (aStack.Count > 0)
-            Console.WriteLine("Top item: " + aStack.Peek());
+            Console.WriteLine("Top item: {0}" + aStack.Peek());
         else
             Console.WriteLine("Stack is empty");
     }
 
-    bool containsSearch = aStack.Contains(search);
-     Console.WriteLine("Stack contains \"{0}\": {1}", search, containsSearch);
+     bool containsSearch = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, containsSearch);
 
     if (containsSearch)
     {
