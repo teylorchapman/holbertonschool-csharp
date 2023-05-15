ea60e388ea43fc844658612e165ab397bd17765b
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 17:18:03 -0500

    fuck comments frfr

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -7,12 +7,12 @@ public class MyStack
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         //print count
-        Console.WriteLine("Number of items: {0}" + aStack.Count);
+        Console.WriteLine("Number of items: " + aStack.Count);
 
     
         //prints whether or not the stack is empy
         if (aStack.Count > 0)
-            Console.WriteLine("Top item: {0}" + aStack.Peek());
+            Console.WriteLine("Top item: " + aStack.Peek());
         else
             Console.WriteLine("Stack is empty");
     
