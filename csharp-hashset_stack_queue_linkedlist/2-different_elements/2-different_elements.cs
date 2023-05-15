e75816254357e026cd1365fccda8417a29bad89d
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 11:33:51 -0500

    different elements

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> differingElements = new List<int>();
+        {
+            foreach (int element in list1)
+            {
+                if (!list2.Contains(element) && !differingElements.Contains(element))
+                {
+                    differingElements.Add(element);
+                }
+            }
+
+            foreach (int element in list2)
+            {
+                if (!list1.Contains(element) && !differingElements.Contains(element))
+                {
+                    differingElements.Add(element);
+                }
+            }
+            differingElements.Sort();
+
+            return differingElements;
+        }
+    }
+}
