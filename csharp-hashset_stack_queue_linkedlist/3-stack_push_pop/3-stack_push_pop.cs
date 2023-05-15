b7e8e04ef7f4e42586f17d9b398ca6c955b39576
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 12:02:28 -0500

    3-stack_push_pop/

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aStack.Count);
+
+        if (aStack.Count > 0)
+            Console.WriteLine("Top item: " + aStack.Peek()):
+        else
+            Console.WriteLine("Stack is empty");
+
+        Console.WriteLine("Stack contains " + search + ": " + aStack.Contains(search));
+
+        if (aStack.Contains(search))
+        {
+            Stack<string> tempStack = new Stack<string>();
+
+            while (aStack.Count > 0)
+            {
+                string item = aStack.Pop();
+
+                if (item == search)
+                break;
+
+                tempStack.Push(item);
+            }
+            while (tempStack.Count > 0)
+            {
+                aStack.Push(tempStack.Pop());
+            }
+        }
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
