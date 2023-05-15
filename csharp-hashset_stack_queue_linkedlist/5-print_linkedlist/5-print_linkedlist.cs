f758b809beaa8c9719e1e17e77f8648e87620425
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 18:21:35 -0500

    linked list

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> sum = new LinkedList<int>();
+        
+        for (int i = 0; i < size; i++)
+        {
+            sum.AddLast(i);
+            Console.WriteLine(i);
+        }
+        return sum;
+    }
+}
