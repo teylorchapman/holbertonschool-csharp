bf5063af3dccef887c2ac7e5e00ea447e4c3f423
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 21:47:46 -0500

    im so done lolllllllllllllllllllll

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index >= 0 && index < myLList.Count)
+        {
+            int currentIndex = 0;
+            LinkedListNode<int> currentNode = myLList.First;
+
+            while (currentNode != null)
+            {
+                if (currentIndex == index)
+                {
+                    myLList.Remove(currentNode);
+                    break;
+                }
+
+                currentNode = currentNode.Next;
+                currentIndex++;
+            }
+        }
+    }
+}
