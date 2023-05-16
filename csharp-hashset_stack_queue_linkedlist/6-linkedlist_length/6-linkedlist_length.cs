fd88f997988b4a956585c41e8a6bb798c4f9c187
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 20:18:49 -0500

    LiNkEd LiSt

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int Length(LinkedList<int> myList)
+    {
+        int i = 0;
+        foreach (int y in myList)
+        {
+            i++;
+        }
+        return i;
+    }
+}
