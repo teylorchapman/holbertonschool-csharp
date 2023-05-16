24cf99f2797dcdcc0476282166c5812ac4fafd3e
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 21:39:00 -0500

    add

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> add = null, node = myLList.First;
+
+        while(node != null)
+        {
+            if (n <= node.Value && (add == null || n > last.Value))
+                return myLList.AddBefore(node,n);
+            add = node;
+            node = node.Next;
+        }
+        return myLList.AddLast(n);
+    }
+}
