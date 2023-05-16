630bd8efa88b8047f239b192f8ab9ffd900e517b
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 20:37:42 -0500

    pop goes the weasle

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList == NULL || myLList.Count == 0)
+            return 0;
+        
+        int i = myLList.First.Value;
+        myLList.RemoveFirst();
+    }
+}
