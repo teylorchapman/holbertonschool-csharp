a2efca5d5a17cd1d6f356ba40f54ad17de80437a
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 20:32:30 -0500

    find

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int sum = -1;
+
+        foreach (int i in myLList)
+        {
+            sum++;
+            if (i == value)
+                return sum;
+        }
+        return -1;
+    }
+}
