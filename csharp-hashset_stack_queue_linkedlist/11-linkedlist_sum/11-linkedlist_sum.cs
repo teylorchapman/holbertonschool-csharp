6856b71fd8c4a2173f2205d3f7483a9e45838f6f
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 21:34:34 -0500

    so close

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int i = 0;
+        foreach (int j in myLList)
+            i += j;
+        return i;
+    }
+}
