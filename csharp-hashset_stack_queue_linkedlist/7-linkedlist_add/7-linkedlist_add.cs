9eb2b49570a825c04103015506435bd1d81a90a9
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 20:23:07 -0500

    adding to the llist

---
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        myLList.AddFirst(n);
+        return myLList.First;
+    }
+}
