b54d29d75594ea27b354dda0ebafd5086ca1e62d
Teylor Chapman <5535@holbertonstudents.com>
2023-05-15 21:23:43 -0500

    i hope dis works the first time frfr

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n >= myLList.Count || n < 0)
+            return 0;
+        
+        var currentNode = myLList.First;
+        for (int i = 0; i < n; i++)
+            currentNode = currentNode.Next;
+        
+        return currentNode.Value;
+    }
+}
