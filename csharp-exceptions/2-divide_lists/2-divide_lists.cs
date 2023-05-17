4318fd7f82f6b0c596b602b2e1a6a6c94c60d8a3
Teylor Chapman <5535@holbertonstudents.com>
2023-05-16 20:58:12 -0500

    divides element by element 2 list

---
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> sum = new List<int>();
+        for (int i = 0; i < listLength; i++)
+        {
+            try
+            {
+                sum.Add(div(list1[i], list2[i]));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+                sum.Add(0);
+            }
+        }
+        return sum;
+    }
+    static int div(int a, int b)
+    {
+        int sum = 0;
+        try
+        {
+            res = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            sum = 0;
+        }
+        return sum;
+    }
+}
