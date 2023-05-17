8818c83dd68a4739590da1c3ffba049a5de24ecc
Teylor Chapman <5535@holbertonstudents.com>
2023-05-16 20:10:10 -0500

    divide print

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Int
+{
+    public static void Divide(int a, int b)
+    {
+        try
+        {
+            int sum = a / b;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, sum)
+        }
+    }
+}
