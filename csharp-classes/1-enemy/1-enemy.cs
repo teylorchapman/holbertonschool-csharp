bcf3c59b7ba645bdc2285f3d547860cc9fef17b3
Teylor Chapman <5535@holbertonstudents.com>
2023-05-22 14:58:01 -0500

    adding health

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    /// This is an empty public Class
+    ///</summary>
+    public class Zombie
+    {
+        ///<summary>
+        /// int health
+        ///</summary>
+
+        public int health;
+
+        ///<summary>
+        /// default constructor
+        /// </summary>
+
+        public Zombie()
+        {
+            
+        }
+    }
+}
