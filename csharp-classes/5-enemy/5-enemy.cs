7f4315d43b218c665644af1212e89575a299691c
Teylor Chapman <5535@holbertonstudents.com>
2023-05-22 16:10:33 -0500

    zombie attributes

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,77 @@
+﻿﻿using System;
+
+namespace Enemies
+{
+    
+    
+    ///<summary>
+    /// This is an empty public Class.
+    ///</summary>
+    
+    public class Zombie
+    {
+        private int health;
+        private string name;        
+        
+        ///<summary>
+        /// int health = 0
+        ///</summary>
+
+
+        ///<summary>
+        /// public constructor
+        /// </summary>
+
+        public Zombie()
+        {
+
+        }
+
+        ///<summary>
+        /// new public constructor that specifies a value for
+        /// the zombie health.
+        ///</summary>
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+            name = "(No name)";
+        }
+
+
+        ///<summary>
+        /// getting or setting the Zombies name
+        ///</summary>
+
+        public string Name
+        {
+            get { return name == null ? "(No name)" : name; }
+            set { name = value; }
+        }
+
+
+        ///<summary>
+        /// Getter for da health.
+        /// Returns the value of health.
+        ///</summary>
+
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        
+        ///<summary>
+        /// overrides ToString() to show the Zombie attributes
+        ///</summary>
+
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+    }
+}
