ffa57efa916496402e483fc3ced2f3d77af5773c
Teylor Chapman <5535@holbertonstudents.com>
2023-05-22 15:05:18 -0500

    health must be greater than 0, duh

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    /// This is an empty public Class
+    ///</summary>
+    public class Zombie
+    {
+        ///<summary>
+        /// int health = 0
+        ///</summary>
+
+        public int health;
+
+        ///<summary>
+        /// public constructor
+        /// </summary>
+
+        public Zombie()
+        {
+
+        }
+
+        ///<summary>
+        /// new public constructor that specifies a value for
+        /// the zombie health
+        ///</summary>
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be great than or equal to 0")
+            }
+        }
+    }
+}
