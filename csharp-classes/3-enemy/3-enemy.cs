fb225c009e57bf6ed81bb46e35a70a6332416773
Teylor Chapman <5535@holbertonstudents.com>
2023-05-22 15:22:28 -0500

    gimmie health info

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    /// This is an empty public Class.
+    ///</summary>
+    public class Zombie
+    {
+        ///<summary>
+        /// int health = 0
+        ///</summary>
+
+        private int health;
+
+        ///<summary>
+        /// public constructor
+        /// </summary>
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary>
+        /// new public constructor that specifies a value for
+        /// the zombie health.
+        ///</summary>
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+
+        ///<summary>
+        /// Getter for da health.
+        /// Returns the value of health.
+        ///</summary>
+
+        public class int GetHealth()
+        {
+            return health;
+        }
+    }
+}
