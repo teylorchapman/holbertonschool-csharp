8ce6382022bcf87c087a98267f4df3512642a652
Teylor Chapman <5535@holbertonstudents.com>
2023-05-22 15:39:06 -0500

    enemy name

---
+++ csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    /// This is an empty public Class.
+    ///</summary>
+    public class Zombie
+    {
+        ///<summary>
+        /// int health = 0
+        ///</summary>
+
+        private int health;
+
+        ///<summary>
+        /// public constructor
+        /// </summary>
+
+        public Zombie()
+        {
+            health = 0;
+            name = "(No name)";
+        }
+
+        ///<summary>
+        /// new public constructor that specifies a value for
+        /// the zombie health.
+        ///</summary>
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+            name = "(No name)";
+        }
+
+
+        ///<summary>
+        /// getting or setting the Zombies name
+        ///</summary>
+
+        public string Name
+        {
+            get (return name);
+            set (name = value);
+        }
+
+
+        ///<summary>
+        /// Getter for da health.
+        /// Returns the value of health.
+        ///</summary>
+
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
