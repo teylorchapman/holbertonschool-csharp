94a27aca6680971ed7ffedf205941beb7762203a
Teylor Chapman <5535@holbertonstudents.com>
2023-05-23 18:16:50 -0500

    subclass?

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+///<summary>
+///public class
+///</summary>
+public class Obj
+{
+    ///<summary>
+    ///looks for if its solely a subclass
+    ///</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
