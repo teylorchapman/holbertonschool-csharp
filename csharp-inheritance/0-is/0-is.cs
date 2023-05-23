9aa99ef888bacf33d0f9eeac84ea1f8049197abb
Teylor Chapman <5535@holbertonstudents.com>
2023-05-23 17:48:19 -0500

    return obj if is int

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Obj
+{
+    ///<summary>
+    /// obj class with IsOfTypeInt method
+    ///</summary> 
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
