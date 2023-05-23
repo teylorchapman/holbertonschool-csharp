28f9944ea263b4ad89e7de6f2e152825778591a9
Teylor Chapman <5535@holbertonstudents.com>
2023-05-23 18:02:11 -0500

    array

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+///<summary>
+/// public class
+///</summary>
+public class Obj
+{
+    ///<summary>
+    /// static bool prototype
+    ///</summary> 
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
