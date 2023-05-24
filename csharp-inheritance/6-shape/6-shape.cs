3110f798ee2fa6f6c2f37bd403654a5b4261d9ac
Teylor Chapman <5535@holbertonstudents.com>
2023-05-24 13:05:12 -0500

    rectangle inherits from shape

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+///<summary>
+/// This is a public class
+///</summary>
+public class Shape
+{
+    ///<summary>
+    /// this is gonna throw an exception
+    ///</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+///<summary>
+/// this is a subclass thats gonna inherit from Shape
+///</summary>
+public class Rectangle : Shape
+{
+    ///<summary>
+    /// These are private classes for the width and the height
+    /// for the rectangle
+    ///</summary>
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+    
+    public int Height
+    {
+        get { return height;}
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+}
