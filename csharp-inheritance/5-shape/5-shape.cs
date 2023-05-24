6f0a32276ee13e29fe37ce7dc0c1fe343c4cfefa
Teylor Chapman <5535@holbertonstudents.com>
2023-05-24 12:15:26 -0500

    area not implemented

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+///<summary>
+/// This is a public class
+///</summary>
+public class Shape
+{
+    ///<summary>
+    /// this is gonna throw an exception
+    ///</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
