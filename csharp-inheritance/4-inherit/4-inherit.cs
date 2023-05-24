2878ea72ae563d55ebb4c394f7dadfe27bd605f8
Teylor Chapman <5535@holbertonstudents.com>
2023-05-24 12:01:40 -0500

    dog inherits from animal

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Animal
+{
+    ///<summary>
+    /// Empy base class Animal
+    ///</summary>
+}
+
+public class Dog : Animal
+{
+    ///<summary>
+    ///Empy subclass that inherits from Animal
+    ///</summary>
+}
