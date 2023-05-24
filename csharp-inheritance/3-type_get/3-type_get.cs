0798d10127e5ed143063777a5fd630f290ee243e
Teylor Chapman <5535@holbertonstudents.com>
2023-05-24 11:54:28 -0500

    get type

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+///<summary>
+/// method that prints the available property names
+///</summary>
+public class Obj
+{
+    ///<summary>
+    /// prototype
+    ///</summary>
+    public static void Print(object myObj)
+    {
+        string className = myObj.GetType().Name;
+
+        Console.WriteLine("{0} Properties:", className);
+        foreach (var item in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine(item.Name);
+        }
+
+        Console.WriteLine("{0} Methods:", className);
+        foreach (var item in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
