86a50927b2d3b1152e4600ffa6fa3f6ee619ab0d
Teylor Chapman <5535@holbertonstudents.com>
2023-05-28 21:53:13 -0500

    magnigtuasdf

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+///<summary>
+/// Public class
+///</summary>
+public class VectorMath
+{
+    ///<summary>
+    /// method to calculate & return the length of a vector
+    ///</summary>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length != 3 || vector.Length != 2)
+        {
+            return (-1);
+        }
+
+        double sum = 0;
+        foreach (double x in vector)
+        {
+            sum += x * x;
+        }
+        return Math.Round(Math.Sqrt(sum), 2);
+    }
+}
