bfae96412286ee283646cbe84560a63ebc6b3913
Teylor Chapman <5535@holbertonstudents.com>
2023-05-29 18:27:29 -0500

    dot product

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -10,21 +10,18 @@ public class VectorMath
     ///</summary>
     public static double DotProduct(double[] vector1, double[] vector2)
     {
-        public static double DotProduct(double[] vector1, double[] vector2)
+        if (vector1.Length != vector2.Length ||
+        vector1.Length != 3 && vector1.Length != 2 || 
+        vector2.Length != 3 && vector2.Length != 2)
         {
-            if (vector1.Length != vector2.Length ||
-            vector1.Length != 3 && vector1.Length != 2 || 
-            vector2.Length != 3 && vector2.Length != 2)
-            {
-                return (-1);
-            }
-
-            double sum = 0;
-            for (int i = 0; i < vector1.Length; i++)
-            {
-                result += vector1[i] * vector2[i];
-            }
-            return sum;
+            return (-1);
         }
+
+        double sum = 0;
+        for (int i = 0; i < vector1.Length; i++)
+        {
+           result += vector1[i] * vector2[i];
+        }
+        return sum;
     }
 }
