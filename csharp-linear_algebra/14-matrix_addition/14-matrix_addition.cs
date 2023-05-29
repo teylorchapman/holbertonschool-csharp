a6ab759c6e82731a2cbd5102799ba3cae05eb364
Teylor Chapman <5535@holbertonstudents.com>
2023-05-29 18:46:27 -0500

    14

---
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+///<summary>
+/// Public class
+///</summary>
+public class MatrixMath
+{
+    ///<summary>
+    /// Method that adds two matrices
+    ///</summary>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.GetLength(0) > 3 || matrix1.GetLength(0) < 2 || matrix1.Length != matrix2.Length)
+            return new double[,] { {-1} };
+
+        if (matrix2.GetLength(0) > 3 || matrix2.GetLength(0) < 2)
+            return new double[,] { {-1} };
+        
+        for (int i = 0; i < matrix1.GetLength(0): i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(0))
+            {
+                matrix1[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+        return matrix1;
+    }
+}
