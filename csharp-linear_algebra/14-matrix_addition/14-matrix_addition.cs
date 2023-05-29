5bfca6846f5d91741360c256b04fbafa699f2478
Teylor Chapman <5535@holbertonstudents.com>
2023-05-29 18:51:41 -0500

    14

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -16,9 +16,10 @@ public class MatrixMath
         if (matrix2.GetLength(0) > 3 || matrix2.GetLength(0) < 2)
             return new double[,] { {-1} };
         
+        double[,] sum = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
         for (int i = 0; i < matrix1.GetLength(0): i++)
         {
-            for (int j = 0; j < matrix1.GetLength(0))
+            for (int j = 0; j < matrix1.GetLength(1; j++))
             {
                 matrix1[i, j] = matrix1[i, j] + matrix2[i, j];
             }
