fd904386463016caf5dcfa8a297b915ba7c1be5a
Teylor Chapman <5535@holbertonstudents.com>
2023-05-29 14:32:48 -0500

    6

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+///<summary>
+/// Public Class
+///</summary>
+public class VectorMath
+{
+    ///<summary>
+    /// Method that adds two vectors
+    ///</summary>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != vector2.Length ||
+        vector1.Length != 3 && vector1.Length != 2 ||
+        vector2.Length != 3 && vector2.Length ! = 2)
+        {
+            return (new Double[1]{-1});
+        }
+
+        double sum = new Double[3];
+        for (int x = 0; x < vector1.Length; x++)
+        {
+            sum[x] = vector1[x] + vector2[x];
+        }
+        return sum;
+    }
+}
