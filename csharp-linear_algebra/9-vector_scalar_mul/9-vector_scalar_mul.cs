605c52a5d36ae5294ec412d2da3a846ebde81cfe
Teylor Chapman <5535@holbertonstudents.com>
2023-05-29 18:07:23 -0500

    9

--- csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -7,6 +7,7 @@ public class VectorMath
 {
     ///<summary>
     /// Method that multiplies a vector and a scalar
+    ///</summary>
     public static double[] Multiply(double[] vector, double scalar)
     {
         if (vector.Length != 3 && vector.Length != 2)
@@ -15,7 +16,7 @@ public class VectorMath
         }
 
         double[] sum = new double[vector.Length];
-        for (int x = 0; i < vector.Length; x++)
+        for (int x = 0; x < vector.Length; x++)
         {
             sum[x] = vector[x] * scalar;
         }
