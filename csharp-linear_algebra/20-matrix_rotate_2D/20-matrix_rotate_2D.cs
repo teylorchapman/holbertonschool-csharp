3f9668c258b6f293d3135fee75063e0ea72e59d2
Teylor Chapman <5535@holbertonstudents.com>
2023-05-30 11:26:35 -0500

    20

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+///<summary>
+/// Public Class
+///</summary>
+public class MatrixMath
+{
+    ///<summary>
+    /// Method that rotates a square 2d matrix
+    ///</summary>
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        rows = matrix.GetLength(0);
+        columns = matrix.GetLength(1);
+
+        if (rows != columns)
+            return new double[,] { {-1} };
+        
+        double[,] rotatedMatrix = new double[rows, columns];
+        double cosAngle = Math.Cos(angle);
+        double sinAngle = Math.Sin(angle);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns, j++)
+            {
+                double x = i - (rows - 1) / 2.0;
+                double y = j - (columns - 1) / 2.0;
+
+                double rotatedX = x * cosAngle - y * sinAngle;
+                double rotatedY = x * sinAngle + y * cosAngle;
+
+                rotatedX += (rows - 1) / 2.0;
+                rotatedY += (columns - 1) / 2.0;
+
+                rotatedMatrix[i, j] = Math.Round(matrix[(int)rotatedX, (int)rotatedY]);
+            }
+        }
+        return rotatedMatrix;
+    }
+}
