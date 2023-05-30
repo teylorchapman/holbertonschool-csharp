ec07362098fab49ed715e5fb9876e3ffd07ead25
Teylor Chapman <5535@holbertonstudents.com>
2023-05-30 13:29:21 -0500

    queue dont look like a real word

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+///<summary>
+/// Public Class
+///</summary>
+public class Queue<T>
+{
+    ///<summary>
+    /// New method that returns the Queue type
+    ///</summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
