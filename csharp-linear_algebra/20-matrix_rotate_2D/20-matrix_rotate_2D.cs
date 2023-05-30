a78e979c404d182521f92a7d0ae15c7c0fb6443b
Teylor Chapman <5535@holbertonstudents.com>
2023-05-30 12:51:02 -0500

    23

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,41 +1,43 @@
-﻿using System;
-
-///<summary>
-/// Public Class
-///</summary>
-public class MatrixMath
+﻿﻿using System;
+using System.Collections;
+    
+/// <summary>Create a method that rotates a square 2D matrix by a given angle in radians and returns the resulting matrix</summary>
+class MatrixMath
 {
-    ///<summary>
-    /// Method that rotates a square 2d matrix
-    ///</summary>
+    /// <summary>Create a method that rotates a square 2D matrix by a given angle in radians and returns the resulting matrix</summary>
+    /// <returns>If the matrix is of an invalid size, return a matrix containing -1</returns>
+    /// <param name="matrix">: Matrix to rotate. </param>
+    /// <param name="angle">: Angle to rotate the matrix. </param>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        int rows = matrix.GetLength(0);
-        int columns = matrix.GetLength(1);
+        double cos = Math.Cos(angle);
+        double sin = Math.Sin(angle);
+        double[,] rotateMatrix = new double[,] {
+            {cos, sin},
+            {-sin, cos}
+        };
 
-        if (rows != columns)
-            return new double[,] { {-1} };
-        
-        double[,] rotatedMatrix = new double[rows, columns];
-        double cosAngle = Math.Cos(angle);
-        double sinAngle = Math.Sin(angle);
-
-        for (int i = 0; i < rows; i++)
+        if (matrix is double[,] && matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
         {
-            for (int j = 0; j < columns; j++)
+            int rowM1 = matrix.GetLength(0);
+            int colM1 = matrix.GetLength(1);
+            int colM2 = rotateMatrix.GetLength(1);
+            int rowM2 = rotateMatrix.GetLength(0);
+
+            double[,] mulMatrix = new double[rowM1, colM2];
+
+            for (int col = 0; col < colM1; col++)
             {
-                double x = i - (rows - 1) / 2.0;
-                double y = j - (columns - 1) / 2.0;
-
-                double rotatedX = x * cosAngle - y * sinAngle;
-                double rotatedY = x * sinAngle + y * cosAngle;
-
-                rotatedX += (rows - 1) / 2.0;
-                rotatedY += (columns - 1) / 2.0;
-
-                rotatedMatrix[i, j] = Math.Round(matrix[(int)rotatedX, (int)rotatedY]);
+                for (int row = 0; row < rowM1; row++)
+                {
+                    for (int ixj = 0; ixj < colM2; ixj++)
+                    {
+                        mulMatrix[row, ixj] = Math.Round(mulMatrix[row, ixj] + matrix[row, col] * rotateMatrix[col, ixj], 2);
+                    }
+                }
             }
+            return mulMatrix;
         }
-        return rotatedMatrix;
+        else  { return new double[,]{{-1}}; }
     }
 }
