493831d68f062a56e7259162d33ebdf6969915cb
Teylor Chapman <5535@holbertonstudents.com>
2023-05-30 09:36:30 -0500

    16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+///<summary>
+/// Public Class
+///</summary>
+public class MatrixMath
+{
+    ///<summary>
+    /// Method that multiplies a maxtrix and a scalar
+    ///</summary>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (matrix.GetLength(0) > 3 || matrix.GetLength(0) < 2)
+            return new double[,] { {-1} };
+        if (matix.GetLength(1) > 3 || matix.GetLength(1) < 2)
+            return new double[,] { {-1} };
+        
+        double sum = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        for (i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (j = 0; j < matrix.GetLength(1); j++)
+            {
+                sum[i, j] = matrix[i, j] * scalar;
+            }
+            return sum;
+        }
+    }
+}
