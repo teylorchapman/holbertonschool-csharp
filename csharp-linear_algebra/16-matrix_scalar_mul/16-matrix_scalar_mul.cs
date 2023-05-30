4b099d8c5aa58377cf3620e59ab49404f94b0137
Teylor Chapman <5535@holbertonstudents.com>
2023-05-30 09:43:14 -0500

    16

--- csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -12,10 +12,10 @@ public class MatrixMath
     {
         if (matrix.GetLength(0) > 3 || matrix.GetLength(0) < 2)
             return new double[,] { {-1} };
-        if (matrix.GetLength(1) > 3 || matix.GetLength(1) < 2)
+        if (matrix.GetLength(1) > 3 || matrix.GetLength(1) < 2)
             return new double[,] { {-1} };
         
-        double sum = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        double[,] sum = new double[matrix.GetLength(0), matrix.GetLength(1)];
         for (i = 0; i < matrix.GetLength(0); i++)
         {
             for (j = 0; j < matrix.GetLength(1); j++)
