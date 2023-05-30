537b737bc8f6641d6c3fdeb8d5395e6c495e0a53
Teylor Chapman <5535@holbertonstudents.com>
2023-05-30 12:47:35 -0500

    20

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -10,43 +10,32 @@ public class MatrixMath
     ///</summary>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        angle *= -1;
-        
-        if (matrix is double[,] && matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
-            return new double[,] { {-1} };
-        
-        double[,] sum = new double[2, 2], rotate = new double[2,2] ;
-        rotate[0, 0] =  Math.Cos(angle);
-        rotate[0, 1] = -1 * Math.Sin(angle);
-        rotate[1, 0] = Math.Sin(angle);
-        rotate[1, 1] = Math.Cos(angle);
-        
         int rows = matrix.GetLength(0);
         int columns = matrix.GetLength(1);
 
-        double[,] results = new double[rows, columns];
+        if (rows != columns)
+            return new double[,] { {-1} };
+        
+        double[,] rotatedMatrix = new double[rows, columns];
+        double cosAngle = Math.Cos(angle);
+        double sinAngle = Math.Sin(angle);
 
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < columns; j++)
             {
-                double sum = 0;
-                for (int k = 0; k < 2; k++)
-                {
-                    sum += matrix[i, k] * rotate[k, j];
-                }
-                results[i, j] = sum;
-            }
-        }
+                double x = i - (rows - 1) / 2.0;
+                double y = j - (columns - 1) / 2.0;
 
-        double[,] results = new double(matrix, rotate);
-        for(int i = 0; i < results.GetLength(0); i++)
-        {
-            for(int j = 0; j < results.GetLength(1); j++)
-            {
-                results[i, j] = Math.Round(results[i, j], 2);
+                double rotatedX = x * cosAngle - y * sinAngle;
+                double rotatedY = x * sinAngle + y * cosAngle;
+
+                rotatedX += (rows - 1) / 2.0;
+                rotatedY += (columns - 1) / 2.0;
+
+                rotatedMatrix[i, j] = Math.Round(matrix[(int)rotatedX, (int)rotatedY]);
             }
         }
-        return results;
+        return rotatedMatrix;
     }
 }
