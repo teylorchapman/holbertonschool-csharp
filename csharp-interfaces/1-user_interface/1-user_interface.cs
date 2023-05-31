85eefd2ee74bb1f4774f2acd44ed13904f756740
Teylor Chapman <5535@holbertonstudents.com>
2023-05-31 10:33:21 -0500

    1.13

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -56,7 +56,7 @@ public class TestObject : Base, IInteractive, IBreakable, ICollectable
     public bool isCollected { get; set; }
 
     ///<summary> This will hopefully fix my issue of not giving name w the main.cs </summary>
-    private string _name
+    private string _name;
     public string name
     {
         get { return _name; }
