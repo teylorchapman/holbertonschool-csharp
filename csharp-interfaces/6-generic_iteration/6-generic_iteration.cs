32d1a49bbc5e2cd8e53d45133cefbf3a7cb91178
Teylor Chapman <5535@holbertonstudents.com>
2023-05-31 17:36:59 -0500

    6

---
+++ csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+///<summary>
+/// IInteractive interface
+///</summary>
+public interface IInteractive
+{
+    ///<summary> Interact method </summary>
+    void Interact();
+}
+
+///<summary>
+/// IBreakable interface
+///</summary>
+public interface IBreakable
+{
+    ///<summary> Property </summary>
+    int durability { get; set; }
+    ///<summary> Break Method </summary>
+    void Break();
+}
+
+///<summary>
+/// ICollectable interface
+///</summary>
+public interface ICollectable
+{
+    ///<summary> Property </summary>
+    bool isCollected { get; set; }
+    ///<summary> Collect method </summary>
+    void Collect();
+}
+
+///<summary>
+/// Public Abstract Class
+/// </summary>
+public abstract class Base
+{
+    ///<summary> This is the base class that overrides a string </summary>
+    public string name { get; set; }
+    ///<summary> overrides a string> </summary>
+    public override string ToString()
+    {
+         return name + " is a " + this.GetType();
+    }
+}
+///<summary>
+/// Public class that inherits from Base and IInteractive
+///</summary>
+public class Door : Base, IInteractive
+{
+    ///<summary> Constructor </summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    ///<summary> Door interaction </summary>
+    public void Interact()
+    {
+        Console.WriteLine( $"You try to open the {this.name}. It's locked." );
+    }
+}
+
+///<summary>
+/// Deco class
+///</summary>
+public class Decoration : Base, IInteractive, IBreakable
+{
+    ///<summary> Public Bool </summary>
+    public bool isQuestItem = false;
+    ///<summary> Durability Property </summary>
+    public int durability { get; set; }
+
+    ///<summary> Constructor </summary>
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
+    {
+        if (durability <= 0)
+        {
+            throw new Exception("Durability must be greater than 0");
+        }
+        this.name = name;
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
+    }
+
+    ///<summary> Interact method </summary>
+    public void Interact()
+    {
+        if (durability <= 0)
+        {
+            Console.WriteLine($"The {this.name} has been broken.");
+        }
+        else if (isQuestItem)
+        {
+            Console.WriteLine($"You look at the {this.name}. There's a key inside.");
+        }
+        else
+        {
+            Console.WriteLine($"You look at the {this.name}. Not much to see here.");
+        }
+    }
+
+    ///<summary> Break() Method </summary>
+    public void Break()
+    {
+        durability--;
+        if (durability < 0)
+        {
+            Console.WriteLine($"The {this.name} is already broken.");
+        }
+        else if (durability == 0)
+        {
+            Console.WriteLine($"You smash the {this.name}. What a mess.");
+        }
+        else
+        {
+            Console.WriteLine($"You hit the {this.name}. It cracks.");
+        }
+    }
+}
+
+///<summary>
+/// Public class
+///</summary>
+public class Key : Base, ICollectable
+{
+    ///<summary> Property </summary>
+    public bool isCollected { get; set; }
+
+    ///<summary> Constructor </summary>
+    public Key(string name = "Key", bool isCollected = false)
+    {
+        this.name = name;
+        this.isCollected = isCollected;
+    }
+
+    ///<summary> Collect() Method </summary>
+    public void Collect()
+    {
+        if (this.isCollected == false)
+        {
+            this.isCollected = true;
+            Console.WriteLine($"You pick up the {this.name}.");
+        }
+        else
+        {
+            Console.WriteLine($"You have already picked up the {this.name}.");
+        }
+    }
+}
+
+///<summary>
+/// New public class
+///</summary>
+public class Objs<T> : IEnumerable<T>
+{
+    ///<summary> List object </summary>
+    List<T> collection = new List<T>();
+
+    ///<summary> Adding an element method </summary>
+    public void Add(T item)
+    {
+        this.collection.Add(obj);
+    }
+
+    ///<summary> GetEnumerator method </summary>
+    public IEnumerator<T> GetEnumerator()
+    {
+       foreach ( T item in collection)
+       {
+            yield return item;
+       }
+    }
+
+    ///<summary> Yeah this is continuing that^ </summary>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
+
