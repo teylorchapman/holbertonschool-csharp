3d97c5cedbabf99c43e679e28dd13a45e4d163f3
Teylor Chapman <5535@holbertonstudents.com>
2023-05-31 09:20:29 -0500

    0

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+///<summary>
+/// This is a public abstract class
+///</summary>
+public abstract class Base
+{
+    ///<summary>
+    /// Public Property
+    ///</summary>
+    public string name;
+
+    ///<summary> 
+    /// Method that will override and return a string
+    ///</summary>
+    public override string ToString()
+    {
+        return name + "{name} is a {type}";
+    }
+}
