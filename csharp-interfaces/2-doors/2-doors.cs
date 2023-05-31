bc882de762b34f2149dea28ae1bc7e4dab2f0964
Teylor Chapman <5535@holbertonstudents.com>
2023-05-31 11:27:52 -0500

    2

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+///<summary>
+/// IInteractive interface
+///</summary>
+public interface IInteractive
+{
+    ///<summary> Interact method </summary>
+    void Interact()
+    {
+        Console.WriteLine( $"You try to open the {this.name}. It's locked." )
+    }
+}
+
+///<summary>
+/// Public Abstract Class
+/// </summary>
+public abstract class Base
+{
+    ///<summary> This is the base class that overrides a string </summary>
+    public string name { get; set; }
+    ///<summary> overrides a string> </summary>
+    public override string ToString()
+    {
+         return name + " is a " + this.GetType();
+    }
+}
+
+///<summary>
+/// Public class that inherits from Base and IInteractive
+///</summary>
+public class Door : Base, IInteractive
+{
+    ///<summary> Constructor </summary>
+    public Door(string name = "Door")
+    {
+        this.name = name
+    }
+}
