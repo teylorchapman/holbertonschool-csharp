f21925a36831a29595ab4b28abbef218a6d4ee1a
Teylor Chapman <5535@holbertonstudents.com>
2023-06-01 11:06:18 -0500

    25;

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+///<summary>
+/// Public class
+///</summary>
+public class MatrixMath
+{
+    ///<summary> Prototype </summary>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        if (matrix.Length == 0)
+        {
+            return new double[,] {};
+        }
+
+        double[,] transposeMatrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int column = 0; column < matrix.GetLength(1); column++)
+            {
+                transposeMatrix[column, row] = Math.Round(matrix[row, column], 2);
+            }
+        }
+    }
+}
