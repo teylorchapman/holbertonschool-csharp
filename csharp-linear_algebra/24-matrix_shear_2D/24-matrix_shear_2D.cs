493fd6fef7e3cae06142b9dd055a4b215894fc1b
Teylor Chapman <5535@holbertonstudents.com>
2023-06-01 10:54:35 -0500

    24

---
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+    
+///<summary>
+/// Public class
+///</summary>
+public class MatrixMath
+{
+    ///<summary> Shear Prototype </summary>
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        int shearX = 0, shearY = 0;
+
+        if (direction == 'x') { shearY = 1; }
+        else if (direction == 'y') { shearX = 1; }
+        else { return new double[,]{{-1}}; }
+
+        double[,] shearMatrix = new double[,] { 
+            {1, shearX * factor},
+            {shearY * factor, 1}
+        };
+        
+        if (matrix is double[,] && matrix.GetLength(1) == 2 && matrix.GetLength(0) == 2)
+        {
+            int rowM1 = matrix.GetLength(0);
+            int colM1 = matrix.GetLength(1);
+            int colM2 = shearMatrix.GetLength(1);
+            int rowM2 = shearMatrix.GetLength(0);
+
+            double[,] mulMatrix = new double[rowM1, colM2];
+
+            for (int col = 0; col < colM1; col++)
+            {
+                for (int row = 0; row < rowM1; row++)
+                {
+                    for (int ixj = 0; ixj < colM2; ixj++)
+                    {
+                        mulMatrix[row, ixj] += matrix[row, col] * shearMatrix[col, ixj];
+                    }
+                }
+            }
+            return mulMatrix;
+        }
+        else  { return new double[,]{{-1}}; }
+    }
+}
